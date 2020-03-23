32a2a7b45e18fe3b828db339a1e7274943e8ae99
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2020-03-23 13:13:46 -0400

    Bump the version to 1.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ServerSentEventStage.MixProject do
   def project do
     [
       app: :server_sent_event_stage,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
