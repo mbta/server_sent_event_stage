43c7734cc87cbcf120df1b1d92f5d03a090d65d5
Paul Swartz <paul@paulswartz.net>
2021-05-25 10:21:17 -0400

    chore: add basic ASDF config
    Needed by Elixir CI to install the right versions.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ServerSentEventStage.MixProject do
     [
       app: :server_sent_event_stage,
       version: "1.0.2",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "ServerSentEventStage",
