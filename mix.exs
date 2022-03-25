22eb726389ef409c48a186b3eb94011f8e6fc97c
Maldonado <emaldonado@mbta.com>
2022-03-25 09:25:11 -0400

    chore: version 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ServerSentEventStage.MixProject do
   def project do
     [
       app: :server_sent_event_stage,
-      version: "1.0.2",
+      version: "1.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
