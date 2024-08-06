d8a2c0f50423834e4cf2c4ccdf960c53b5306c6c
Cora Grant <cgrant2@mbta.com>
2024-08-06 10:09:22 -0400

    chore: cut release 1.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ServerSentEventStage.MixProject do
   def project do
     [
       app: :server_sent_event_stage,
-      version: "1.2.0",
+      version: "1.2.1",
       elixir: ">= 1.11.4 and < 2.0.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
